177828202090d6b19e28c17fd63df21285d9a21e
Sukanta <amisukanta02@gmail.com>
2015-03-25 00:39:46 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate
     if !signed_in?
-    	redirect_to new_session_path
+      redirect_to new_session_path
     end
   end
 
